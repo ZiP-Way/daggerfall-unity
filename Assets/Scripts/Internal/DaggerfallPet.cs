87661222f745e6d56f7b422d6f99e491b6d1c464
Maksym <zip.way.dev@gmail.com>
2024-02-16 11:11:59 +0200

    - Create DaggerfallPet class;

---
+++ Assets/Scripts/Internal/DaggerfallPet.cs
@@ -0,0 +1,7 @@
+﻿namespace DaggerfallWorkshop
+{
+    /// <summary>
+    /// Stores pet settings for serialization and other tasks.
+    /// </summary>
+    public class DaggerfallPet : DaggerfallCharacter { }
+}
