745c9237bf4283e4fad4428d5f1f8f3db798dbc3
Maksym <zip.way.dev@gmail.com>
2024-02-16 13:10:39 +0200

    - Implement ability to spawn a pet when the player spawned to start of the dungeon;

---
+++ Assets/Scripts/Game/Player/PetSpawner.cs
@@ -0,0 +1,25 @@
+﻿using DaggerfallWorkshop;
+using DaggerfallWorkshop.Game;
+using DaggerfallWorkshop.Utility;
+using UnityEngine;
+
+namespace Game.Player
+{
+    public class PetSpawner : MonoBehaviour
+    {
+        [SerializeField]
+        private string _name = "Johny";
+
+        [SerializeField]
+        private MobileTypes _type;
+
+        private void OnEnable() =>
+            PlayerEnterExit.OnMovePlayerToDungeonStart += SpawnPet;
+
+        private void OnDisable() =>
+            PlayerEnterExit.OnMovePlayerToDungeonStart -= SpawnPet;
+
+        private void SpawnPet() =>
+            GameObjectHelper.CreatePet(_name, _type, transform.position);
+    }
+}
