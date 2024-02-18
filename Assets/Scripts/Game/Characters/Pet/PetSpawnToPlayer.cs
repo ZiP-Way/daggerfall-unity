8004a243fe02ff8e9e9ab9cdc9319634591117cb
Maksym <zip.way.dev@gmail.com>
2024-02-18 15:39:34 +0200

    - Implement ability to spawn a pet near the player if the pet is far from the player

---
+++ Assets/Scripts/Game/Characters/Pet/PetSpawnToPlayer.cs
@@ -0,0 +1,29 @@
+﻿using DaggerfallWorkshop.Game;
+using UnityEngine;
+
+namespace Game.Characters.Pet
+{
+    public class PetSpawnToPlayer : MonoBehaviour
+    {
+        [SerializeField]
+        private float _maxDistance;
+
+        private void LateUpdate()
+        {
+            if (IsPetFarFromPlayer())
+                SpawnPetToPlayer();
+        }
+
+        private bool IsPetFarFromPlayer()
+        {
+            Vector3 playerPosition = GameManager.Instance.PlayerObject.transform.position;
+            return Vector3.Distance(transform.position, playerPosition) > _maxDistance;
+        }
+
+        private void SpawnPetToPlayer()
+        {
+            Vector3 playerPosition = GameManager.Instance.PlayerObject.transform.position;
+            transform.position = playerPosition;
+        }
+    }
+}
